db2dd8c839d2c3205726563338711df55f7552c4
K7theCompSciMan <k7rang2018@gmail.com>
2023-07-12 17:29:09 -0400

    Created New Svelte Project with tailwindcss

---
+++ Web Dev Stuff/Svelte/Wordle/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class=" text-center ">
+    <h1 class=" text-3xl pb-10">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>    
+</div>
