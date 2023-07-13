4a13adfb1a5ca3443416178569ac1cd6453748bf
Kesavan Rangarajan <129635995+K7theCompSciMan@users.noreply.github.com>
2023-07-13 11:09:41 -0400

    vercel test2

--- Web Dev Stuff/Svelte/Wordle/src/routes/+page.svelte
+++ Web Dev Stuff/Svelte/Wordle/src/routes/+page.svelte
@@ -2,5 +2,5 @@
     <h1 class=" text-3xl pb-10">Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>    
 
-    <p>Hello Vercel</p>
+    <p class="text-red-500">Hello Vercel</p>
 </div>
