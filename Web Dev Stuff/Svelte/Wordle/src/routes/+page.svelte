db1e372d4d2c8630610da826cbf70184a4b00f2d
K7theCompSciMan <k7rang2018@gmail.com>
2023-07-14 20:17:39 -0400

    Started work on wordle project

--- Web Dev Stuff/Svelte/Wordle/src/routes/+page.svelte
+++ Web Dev Stuff/Svelte/Wordle/src/routes/+page.svelte
@@ -1,6 +1,66 @@
-<div class=" text-center ">
-    <h1 class=" text-3xl pb-10">Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>    
+<script lang='ts'>
+    let guesses = ['','','','','',''];
+    let currentGuess = 1;
+    let currentLetter = 1;
 
-    <p class="text-red-500 text-3xl">Hello Vercel</p>
+    $: console.log(guesses);
+
+</script>
+
+<svelte:head>
+    <title>Wordle by K7</title>
+    <meta name="description" content="Wordle by K7">
+    <meta name="keywords" content="Wordle, K7">
+    <meta name="author" content="K7">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
+
+
+<div class="bg-black flex justify-center font-medium w-screen h-screen text-white">
+    <h1 class="text-xl mt-32 h-10">Wordle by K7</h1>
+    <div class="bg-blue-500 w-1/4 h-1/2 text-center justify-center absolute top-48 rounded-lg">
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+            <div class="border relative w-full mt-4 h-16 items-center justify-center flex">
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+                <div class="p-2 text-lg border-black border-2 w-16 h-10 flex justify-center items-center mr-1"></div>
+            </div>
+        </div>
 </div>
+
